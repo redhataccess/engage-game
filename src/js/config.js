c65d47049c1904af57649851d1124455a5277f6f
mwcz <mwc@clayto.com>
2017-04-05 14:20:02 -0400

    clean up config file

--- src/js/config.js
+++ src/js/config.js
@@ -15,13 +15,16 @@ const config = Object.freeze({
     INPUT_WAIT_MS: Phaser.Timer.SECOND * 0.5,
 
     // how long to wait after game begins before first event falls from sky
-    COFFEE_DELAY_MS: 3000,
+    COFFEE_DELAY_MS: 3 * Phaser.Timer.SECOND,
 
     // how long the "day" lasts
     DAY_DURATION_MS: 50 * Phaser.Timer.SECOND,
 
     // how long to wait after the last block falls before ending the game
     END_DURATION_MS: 3 * Phaser.Timer.SECOND,
+
+    // how long to wait after game over before restarting
+    GAME_OVER_RESTART_DURATION_MS: 7 * Phaser.Timer.SECOND,
 
     // vulns per day
     VULNS_PER_DAY: 5,
@@ -30,7 +33,7 @@ const config = Object.freeze({
     BLOCK_GRAVITY: 300,
 
     // minimum interval between dropping blocks
-    BLOCK_DROP_MIN_INTERVAL_MS: 100,
+    BLOCK_DROP_MIN_INTERVAL_MS: 0.1 * Phaser.Timer.SECOND,
 
     // how quickly should block drops ramp up?  any easing function can be used
     BLOCK_DROP_PROBABILITY_FUNC: Phaser.Easing.Quadratic.InOut,
@@ -48,26 +51,16 @@ const config = Object.freeze({
     // (lerped)
     CONTROL_RESPONSIVENESS: 1.0,
 
-    // how long to wait after game over before restarting
-    GAME_OVER_RESTART_DURATION_MS: 7 * Phaser.Timer.SECOND,
-
     // how wide the side chambers should be (Portal chamber on left and Legend
     // chamber on right)
     SIDE_CHAMBER_WIDTH: 250,
 
-    // Min and max number of regular blocks between a vuln block
-    MIN_VULN_GAP: 15,
-    MAX_VULN_GAP: 30,
-
-    // Number of blocks before next CVE drops
-    VULN_TO_CVE_BLOCKS: 15,
-
     // How much to fill up the well with each splashdown, as percentage of game
     // height, from 0 to 1
     WELL_FILL_PER_BLOCK: 0.003,
-    WELL_FILL_DURATION_MS: 100, // how long it takes to tween height after each splashdown
+    WELL_FILL_DURATION_MS: 0.1 * Phaser.Timer.SECOND, // how long it takes to tween height after each splashdown
 
     // How long the lunch score multiplier bonus will last
-    LUNCH_BOOST_DURATION: 7000,
+    LUNCH_BOOST_DURATION: 7 * Phaser.Timer.SECOND,
 
 });
