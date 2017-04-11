8611ab23b84a7f795397be0a958f160192a8fc9e
mwcz <mwc@clayto.com>
2017-04-11 16:44:42 -0400

    add leaderboard server

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A game.
 
 ## Development commands
 
-Start a development environment with BrowserSync:
+Start a development environment:
 
     npm start
 
@@ -16,3 +16,7 @@ BrowserSync will automatically reload your browser when files change.  No more
 manual refreshing!  You can open the game in multiple browsers and they will
 all be refreshed together.  Clicks will also be synchronized so you can test
 the game in several browsers at the same time.
+
+If you also want to run the leaderboard server, run this in another terminal:
+
+    npm run leader-server
--- src/leaderboard/leaderboard.js
+++ src/leaderboard/leaderboard.js
@@ -9,7 +9,7 @@
     leaderboardservice.$inject = ['$http'];
     LeaderboardController.$inject = ['$scope', '$interval', 'leaderboardservice'];
 
-    var serviceUrl = '/leaderboard/leaders.json',
+    var serviceUrl = 'http://localhost:1337/parse/classes/leaders',
         maxLeaders = 10,
         updateInterval = 5000;
 
@@ -30,21 +30,32 @@
         };
 
         function getLeaders() {
-            return $http.get(serviceUrl)
+            return $http.get(
+                serviceUrl,
+                {
+                    headers: {
+                        'X-Parse-Application-Id': 'ENGAGE'
+                    },
+                })
                 .then(getLeadersComplete)
                 .catch(getLeadersFailed);
 
             function getLeadersComplete(response) {
-                /*
-                 * crazy one liner
-                 */
-                return _.take(_.filter(_.sortByOrder(_.toArray(response.data), 'ping.hiscore', false), 'ping.hiscore'), maxLeaders);
+                const leaders = _(response.data.results)
+                    .sortBy('score')
+                    .reverse()
+                    .uniqBy('name')
+                    .take(10)
+                    .value();
+
+                return leaders;
             }
 
             function getLeadersFailed(error) {
                 /*
                  * there was an error
                  */
+                console.error(error);
             }
         }
     }
