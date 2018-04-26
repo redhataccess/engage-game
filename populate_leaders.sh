de4b1a22e813d6d0b85ad3d9d78e28d2ce99e0dc
Jared Sprague <jared@caramelcode.com>
2018-04-26 15:35:41 -0400

    added AccountId to populate_leaders.sh

--- populate_leaders.sh
+++ populate_leaders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"score":1000,"name":"Krystal","email":"kmoore@redhat.com"}' http://localhost:1337/parse/classes/leaders/
-curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"score":900,"name":"Michael","email":"mclayton@redhat.com"}' http://localhost:1337/parse/classes/leaders/
-curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"score":800,"name":"Jared","email":"jsprague@redhat.com"}' http://localhost:1337/parse/classes/leaders/
-curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"score":700,"name":"Lauren","email":""}' http://localhost:1337/parse/classes/leaders/
-curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"score":600,"name":"Allyson","email":""}' http://localhost:1337/parse/classes/leaders/
+curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"AccountId":1,"score":1000,"name":"Krystal","email":"kmoore@redhat.com"}' http://localhost:1337/parse/classes/leaders/
+curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"AccountId":2,"score":900,"name":"Michael","email":"mclayton@redhat.com"}' http://localhost:1337/parse/classes/leaders/
+curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"AccountId":3,"score":800,"name":"Jared","email":"jsprague@redhat.com"}' http://localhost:1337/parse/classes/leaders/
+curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"AccountId":4,"score":700,"name":"Lauren","email":""}' http://localhost:1337/parse/classes/leaders/
+curl -X POST -H "X-Parse-Application-Id: ENGAGE" -H "Content-Type: application/json" -d '{"AccountId":5,"score":600,"name":"Allyson","email":""}' http://localhost:1337/parse/classes/leaders/
